77be94280c915390dacf9c742363c55c20b05eff
mubaarock <mustophamubarack17@gmail.com>
2025-01-30 18:05:33 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from decouple import config
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods="GET")
+def main():
+    response = jsonify(
+        {
+            "email": config("HNG12_EMAIL"),
+            "current_datetime": datetime.utcnow().isoformat(timespec="seconds") + "Z",
+            "github_url": config("GITHUB_URL"),
+        }
+    )
+
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
